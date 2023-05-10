a01891bd19ba9208589ea750c5e549fdc8c5b361
cojocarucristian05 <cojocaru.cristian20@gmail.com>
2023-05-10 19:35:34 +0300

    make changes in post controller

--- twitter/src/main/java/com/ligaaclabs/twitter/controller/PostController.java
+++ twitter/src/main/java/com/ligaaclabs/twitter/controller/PostController.java
@@ -2,6 +2,7 @@ package com.ligaaclabs.twitter.controller;
 
 import com.ligaaclabs.twitter.model.Post;
 import com.ligaaclabs.twitter.service.PostService;
+import com.ligaaclabs.twitter.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +16,12 @@ public class PostController {
     @Autowired
     public final PostService postService;
 
-    public PostController(PostService postService) {
+    @Autowired
+    public final UserService userService;
+
+    public PostController(PostService postService, UserService userService) {
         this.postService = postService;
+        this.userService = userService;
     }
 
 //    @GetMapping("/posts")
